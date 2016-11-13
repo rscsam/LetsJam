d28b106f44f9151eda72435ae4320a6169d11f94
rscsam <sam.costley@yahoo.com>
2016-11-13 08:52:28 -0500

    Initial commit

--- app/src/main/java/com/hack/rss/letsjam/EndJam.java
+++ app/src/main/java/com/hack/rss/letsjam/EndJam.java
@@ -1,8 +1,10 @@
 package com.hack.rss.letsjam;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import java.io.File;
@@ -10,42 +12,42 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
+import static android.R.attr.name;
+
 public class EndJam extends AppCompatActivity {
-
-    //// TODO: 11/13/16 Something in these declarations breaks the whole program
-    /*ArrayList<Integer> record;
-    Context context = getApplicationContext();
-    File path = context.getFilesDir();
-    String name;
-    File file = new File(path, name);
-    DataOutputStream dataOutput;
-
-    EditText input;*/
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_jam);
 
-        //Bundle extras = getIntent().getExtras();
-        /*record = extras.getIntegerArrayList("record");
+
+    }
+
+
+    public void saveJam(View v) {
+        final EditText fileNameET = (EditText) findViewById(R.id.file_name_et);
+        String name = fileNameET.getText().toString();
+        Context context = getApplicationContext();
+        File path = context.getFilesDir();
+        File file = new File(path, name);
+        Bundle extras = getIntent().getExtras();
+        ArrayList<Integer> record = extras.getIntegerArrayList("record");
         FileOutputStream output = null;
         try {
             output = new FileOutputStream(path);
         } catch (java.io.FileNotFoundException e) { finish();}
 
-        dataOutput = new DataOutputStream(output);*/
-    }
-
-
-    /*void saveJam() {
-
+        DataOutputStream dataOutput = new DataOutputStream(output);
         for(Integer integer: record) {
             try {
                 dataOutput.write(integer);
             } catch (java.io.IOException e) {}
-
         }
-    }*/
+    }
+
+    public void returnHome(View view) {
+        Intent startOver = new Intent(this, StartActivity.class);
+        startActivity(startOver);
+        finish();
+    }
 }
